d468ff54fd1341234ece53b05ea28fa5b7c81373
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-07-30 13:25:32 -0500

    Agregando Formulario

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
-// document.getElementById("btn-filtrar").addEventListener('click', (e) =>{
-// 	return 1 + 2
-// }
-// );
+document.getElementById("btn-guardar-inmueble").addEventListener('click', (e) => {
+    if (document.getElementById("frm-mensualidad").reportValidity()) {
+        let nombre, documento, genero, fecha_nac;
+    } else {
+        //alert("Error de validación de campos.")
+    }
+});
+
+
+
 
 $(document).ready((e) => {
     $("#btn-adicionar").click((e) => {
         $('#modal-adicionar').modal('toggle');
     });
 });
-
