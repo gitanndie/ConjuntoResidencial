a86ae09abcc7dfe8abea351a4098137c73197928
Anndie Gonzalez <anndiegonzalez@gmail.com>
2020-07-30 12:19:45 -0500

    Agregando js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-document.getElementById("btn-filtrar").addEventListener('click', (e) => alert("Filtrar"))
+// document.getElementById("btn-filtrar").addEventListener('click', (e) =>{
+// 	return 1 + 2
+// }
+// );
+
+$(document).ready((e) => {
+    $("#btn-adicionar").click((e) => {
+        $('#modal-adicionar').modal('toggle');
+    });
+});
+
